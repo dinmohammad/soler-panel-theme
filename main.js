05257ec2a72e8f342ae4bf50d556a4b70e944641
Din Mohammad <din.aitvet@gmail.com>
2023-05-13 23:57:24 +0600

    section done

---
+++ main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".testimonials_slider", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
+});
